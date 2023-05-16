36a4a21bfd891ee56c315668eabeb6e02085fe97
MWEA\Ichraks <Ichrak.smati@maibornwolff.de>
2023-05-16 13:49:46 +0100

    feat project structure

---
+++ school-core/src/test/java/anywr/recruitment/schoolCore/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package anywr.recruitment.schoolCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
